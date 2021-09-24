eab953049290bc888c5706bec6efbcadcdc11f15
Eizany <50945740+eizany@users.noreply.github.com>
2021-09-25 02:47:57 +1000

    Added input boxes/buttons/calendars
    Data passes to user_period when button is pressed
    
    Needs min/max for calendars

--- functions/user_period.py
+++ functions/user_period.py
@@ -1,3 +1,4 @@
+from PyQt5.QtCore import QDate, QDateTime
 ### The start of one of the functions? Sep file for each one? or one file for all????
 def find_data(start_date, end_date, search_term):
-    print(f"Hey? {start_date}, {end_date} and search {search_term}")
+    print(f"Search input from home page! START: {start_date.toPyDate()} and END: {end_date.toPyDate()} and the keyword is {search_term}")
--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from functions.user_period import find_data
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.data = open('./data/Crash Statistics Victoria.csv', "r")
+        #self.data = open('./data/Crash Statistics Victoria.csv', "r")
+        self.data = "hi"
         #window settings
         self.setWindowTitle("VicCrash Viewer")
         self.width = 1500
@@ -133,6 +134,79 @@ class Window(QMainWindow):
     #########
 
     def homePage(self):
+        #### Search/date boxes/inputs
+        # "Label"
+        self.keyword_search_box = QGroupBox("Search Term")
+        # The input box, will grab value for search
+        self.keyword_search_input = QLineEdit(self)
+        self.keyword_search_input.setPlaceholderText("Enter Keyword")
+       
+        # Creating a nice 'label' similar to the design documents
+        # Adding the input to the 'label' box
+        self.keyword_search_layout = QVBoxLayout(self.keyword_search_box)
+        self.keyword_search_layout.addStretch(2)
+        self.keyword_search_box.setLayout(self.keyword_search_layout)
+        self.keyword_search_layout.addWidget(self.keyword_search_input)
+        self.keyword_search_box.setStyleSheet("""
+            QGroupBox {
+                color: black;
+            }
+         """)
+
+        # The start date label and input, need to set a 'min' date from the data
+        self.start_date_input_box = QGroupBox("Start Date")
+        # Calendar input, will get date for search
+        self.start_date_input = QDateEdit(calendarPopup=True)
+        self.start_date_input.setDateTime(QtCore.QDateTime.currentDateTime())
+        # "Label"
+        self.start_date_layout = QVBoxLayout(self.start_date_input_box)
+        self.start_date_layout.addStretch(2)
+        self.start_date_input_box.setLayout(self.start_date_layout)
+        self.start_date_layout.addWidget(self.start_date_input)
+        self.start_date_input_box.setStyleSheet("""
+            QGroupBox {
+                color: black;
+            }
+         """)
+
+        # The end date label and input, need to set a 'max' date from the data
+        self.end_date_input_box = QGroupBox("End Date")
+        self.end_date_input = QDateEdit(calendarPopup=True)
+        self.end_date_input.setDateTime(QtCore.QDateTime.currentDateTime())
+        self.end_date_layout = QVBoxLayout(self.end_date_input_box)
+        self.end_date_layout.addStretch(2)
+        self.end_date_input_box.setLayout(self.end_date_layout)
+        self.end_date_layout.addWidget(self.end_date_input)
+        self.end_date_input_box.setStyleSheet("""
+            QGroupBox {
+                color: black;
+            }
+         """)
+        # Search button label and button linked to homePagePerformSearch function, that passes data to user_period.py
+        self.search_box = QGroupBox("Search")
+        self.search_button = QPushButton('Go', self)
+        self.search_button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+        self.search_button.clicked.connect(self.homePagePerformSearch)
+        self.search_layout = QVBoxLayout(self.search_box)
+        self.search_layout.addStretch(2)
+        self.search_box.setLayout(self.search_layout)
+        self.search_layout.addWidget(self.search_button)
+        self.search_box.setStyleSheet("""
+            QGroupBox {
+                color: black;
+            }
+         """)
+
+        # All all the above 'labels' and inputs into a layout ref: labelsandinputs
+        self.search_input_holder = QGroupBox()
+        self.input_button_holders = QGridLayout(self)
+        self.input_button_holders.addWidget(self.keyword_search_box, 0, 0, 1, 2)
+        self.input_button_holders.addWidget(self.start_date_input_box, 0, 2, 1, 1)
+        self.input_button_holders.addWidget(self.end_date_input_box, 0, 3, 1, 1)
+        self.input_button_holders.addWidget(self.search_box, 0, 4, 1, 1)
+        self.search_input_holder.setLayout(self.input_button_holders)
+
+        # Data view 
         self.model = QtGui.QStandardItemModel(self)
         self.tableView = QtWidgets.QTableView(self)
         self.tableView.setMinimumHeight(900)
@@ -152,13 +226,14 @@ class Window(QMainWindow):
         shadow = QGraphicsDropShadowEffect(blurRadius=15, xOffset=1, yOffset=1)
         self.tableView.setGraphicsEffect(shadow)
         tab_layout = QVBoxLayout()
+        
         tab_layout.addWidget(QLabel('Accident Data'))
-        tab_layout.addWidget(self.tableView)
+        tab_layout.addWidget(self.search_input_holder) # This is labelsandinputs being added to the main tab
+        tab_layout.addWidget(self.tableView) # The data view, will need to update this when the search has returned data
         tab_layout.addStretch(5)
         tab = QWidget()
         tab.setLayout(tab_layout)
         return tab
-
 
     def todPage(self):
         tab_layout = QVBoxLayout()
@@ -184,6 +259,16 @@ class Window(QMainWindow):
         tab.setLayout(tab_layout)
         return tab
 
+    #############
+    # Functions #
+    #############
+    def homePagePerformSearch(self):
+        find_data(self.start_date_input.date(), self.end_date_input.date(), self.keyword_search_input.text())
+
+
+###########################
+# Running the application #
+###########################
 if __name__ == '__main__':
     import sys
 
@@ -195,6 +280,5 @@ if __name__ == '__main__':
         }
     """)
     mainApp.show()
-    find_data("20", "20", "Yolo")
 
     sys.exit(app.exec_())
