5f8abf0e528c916f8a1c684105aed71e1f0671b0
Eizany <50945740+eizany@users.noreply.github.com>
2021-09-29 07:14:58 +1000

    Dates fixed, my find_data returning correct count!
    create_db.py will run a conversion on dates to convert
    them from d/m/yyyy to proper yyyy-mm-dd now

--- data/create_db.py
+++ data/create_db.py
@@ -1,12 +1,33 @@
 import sqlite3 as db
 import subprocess
+import time
 
 conn = db.connect('./crash.db') # This automatically creates a database if it does not exist
 cursor = conn.cursor()
+conn.set_trace_callback(print)
 query = "DROP TABLE IF EXISTS crashdata;" # Drop the table each time
 cursor.execute(query)
+conn.close()
 
 subprocess.call(["sqlite3", "crash.db",
 ".mode csv",
 ".import 'Crash Statistics Victoria.csv' crashdata"]) # Import all the data back in 
+time.sleep(2)
 
+newconn = db.connect('./crash.db')
+nc = newconn.cursor()
+newconn.set_trace_callback(print)
+# if date is like 15/5/2019
+# if date is like 5/12/2019
+# if date is like 5/5/2019
+# if date is like 12/12/2019
+# Sets all to proper format of yyyy-mm-dd
+nc.executescript("""
+    UPDATE crashdata SET accident_date = substr(accident_date, 6) || '-' || '0' || substr(accident_date, 4, 1) || '-' || substr(accident_date, 1, 2) WHERE accident_date LIKE '__/_/____';
+    
+    UPDATE crashdata SET accident_date = substr(accident_date, 6) || '-' || substr(accident_date, 3, 2) || '-' || '0' || substr(accident_date, 1, 1) WHERE accident_date LIKE '_/__/____';
+    
+    UPDATE crashdata SET accident_date = substr(accident_date, 5) || '-' || '0' || substr(accident_date, 3, 1) || '-' || '0' || substr(accident_date, 1, 1) WHERE accident_date LIKE '_/_/____';
+    
+    UPDATE crashdata SET accident_date = substr(accident_date, 7) || '-' || substr(accident_date, 4, 2) || '-' || substr(accident_date, 1, 2) WHERE accident_date LIKE '__/__/____';
+""")
--- functions/user_period.py
+++ functions/user_period.py
@@ -1,14 +1,12 @@
 from PyQt5.QtCore import QDate, QDateTime
-### The start of one of the functions? Sep file for each one? or one file for all????
-
 
 
 def find_data(start_date, end_date, search_term, data):
-    startdate = start_date.toString('d/M/yyyy')
-    enddate = end_date.toString('d/M/yyyy')
+    startdate = start_date.toString('yyyy-MM-dd')
+    enddate = end_date.toString('yyyy-MM-dd')
     c = data.cursor()
     data.set_trace_callback(print)
-    c.execute(f"select * from crashdata WHERE region_name LIKE '%{search_term}%'")
+    c.execute(f"select * from crashdata WHERE date(accident_date) BETWEEN date('{startdate}') and date('{enddate}')")
     print(len(c.fetchall()))
 
     # debug to check inputs
